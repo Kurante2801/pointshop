1dba2cc25a01f889268e8895429b41716af538fc
Kurante <kurante2801@gmail.com>
2022-09-05 11:36:40 +0200

    Add bottom right buttons

--- lua/pointshop/vgui_new/dpointshop_menu.lua
+++ lua/pointshop/vgui_new/dpointshop_menu.lua
@@ -12,8 +12,8 @@ surface.CreateFont("PS_Header", {
     size = 30, shadow = false, antialias = true,
 })
 
-surface.CreateFont("PS_SideBar", {
-    font = "Rubik Regular",
+surface.CreateFont("PS_Label", {
+    font = "Rubik SemiBold",
     size = 20, shadow = false, antialias = true,
 })
 
@@ -32,12 +32,10 @@ function PS:FadeHover(panel, color_string, alpha, speed, round)
 end
 
 local activefunc = function(this) return this.Active end
+local downfunc = function(this) return this:IsDown() end
+
 function PS:FadeActive(panel, color_string, alpha, speed, round)
     return self:FadeFunction(panel, "PS_FadeActive", color_string, alpha, speed, round, activefunc)
-end
-
-function PS:ClearPaint(panel)
-    panel:TDLib().Paint = emptyfunc
 end
 
 -- https://github.com/Arizard/deathrun/blob/master/gamemode/cl_derma.lua#L57
@@ -51,7 +49,7 @@ PS.ShadowedText = function(text, font, x, y, color, alignx, aligny, blur)
         draw.SimpleText(text, font, x + blur, y + blur, text_shadow, alignx, aligny)
     end
 
-    draw.SimpleText(text, font, x, y, color, alignx, aligny)
+    return draw.SimpleText(text, font, x, y, color, alignx, aligny)
 end
 
 
@@ -83,6 +81,14 @@ PS.ShadowedImage = function(material, x, y, w, h, color, alignx, aligny, blur)
 
     surface.SetDrawColor(color.r, color.g, color.b, color.a)
     surface.DrawTexturedRect(x, y, w, h)
+end
+
+local foreground1func = function(panel, w, h)
+    draw.RoundedBox(0, 0, 0, w, h, PS:GetThemeVar("Foreground1Color"))
+end
+
+local foreground1roundfunc = function(panel, w, h)
+    draw.RoundedBox(6, 0, 0, w, h, PS:GetThemeVar("Foreground1Color"))
 end
 
 local PANEL = {}
@@ -122,9 +128,7 @@ function PANEL:Init()
     self.Left:Dock(LEFT)
     self.Left.Active = cookie.GetString("PS_SideBar", 0) == "1"
     self.Left:SetWide(self.Left.Active and 46 or 186)
-    self.Left.Paint = function(this, w, h)
-        draw.RoundedBox(0, 0, 0, w, h, PS:GetThemeVar("Foreground1Color"))
-    end
+    self.Left.Paint = foreground1func
 
     self.SideBarToggle = self.Left:Add("DButton")
     self.SideBarToggle:Dock(TOP)
@@ -160,7 +164,41 @@ function PANEL:Init()
     self.Right:SetWide(246)
     self.Right.Paint = emptyfunc
 
+    self.Controls = self.Right:Add("DPanel")
+    self.Controls:Dock(BOTTOM)
+    self.Controls:SetTall(82)
+    self.Controls:DockMargin(0, 6, 6, 6)
+    self.Controls:DockPadding(6, 6, 6, 6)
+    self.Controls.Paint = foreground1roundfunc
 
+    -- Buy button
+    self.Buy = self.Controls:Add("PS_Button")
+    self.Buy:Dock(BOTTOM)
+    self.Buy:SetTall(32)
+    self.Buy:SetText("Purchase")
+    self.Buy:SetIcon("lbg_pointshop/derma/shopping_cart.png", 18, 18)
+    self.Buy:SetHoverText("-1234567890")
+    self.Buy:SetHoverIcon("lbg_pointshop/derma/sell.png", 18, 18)
+
+    -- Top buttons
+    self.ControlsTop = self.Controls:Add("DPanel")
+    self.ControlsTop:Dock(TOP)
+    self.ControlsTop:SetTall(32)
+    self.ControlsTop.Paint = emptyfunc
+
+    -- Customize button
+    self.Customize = self.ControlsTop:Add("PS_Button")
+    self.Customize:Dock(LEFT)
+    self.Customize:SetWide(111)
+    self.Customize:SetText("Customize")
+    self.Customize:SetHoverText("Unavailable")
+
+    -- Equip button
+    self.Equip = self.ControlsTop:Add("PS_Button")
+    self.Equip:Dock(RIGHT)
+    self.Equip:SetWide(111)
+    self.Equip:SetText("Equip")
+    self.Equip:SetHoverText("Holster")
 
     self:PopulateCategories()
 
@@ -225,7 +263,7 @@ function PANEL:PopulateCategories()
             :ClearPaint()
             :Text("")
             :On("PaintOver", function(this, w, h)
-                PS.ShadowedText(cat.Name, "PS_SideBar", h, h * 0.5, COLOR_WHITE, TEXT_ALIGN_LEFT, TEXT_ALIGN_CENTER)
+                PS.ShadowedText(cat.Name, "PS_Label", h, h * 0.5, COLOR_WHITE, TEXT_ALIGN_LEFT, TEXT_ALIGN_CENTER)
             end)
             :On("DoClick", function(this)
                 if this.Active then return end
@@ -261,4 +299,143 @@ function PANEL:HidePanels()
 end
 
 vgui.Register("PS_Menu", PANEL, "DFrame")
---vgui.Create("PS_Menu")
+
+PANEL = {}
+AccessorFunc(PANEL, "_color", "ThemeColor", FORCE_STRING)
+AccessorFunc(PANEL, "_hovertext", "HoverText", FORCE_STRING)
+
+PANEL.AlignX = TEXT_ALIGN_CENTER
+PANEL.AlignY = TEXT_ALIGN_CENTER
+
+function PANEL:Init()
+    self.SetTextOriginal = self.SetText
+    self.SetText = self.SetTextOverride
+
+    self.SetContentAlignmentOriginal = self.SetContentAlignment
+    self.SetContentAlignment = self.SetContentAlignmentOverride
+
+    self:SetFont("PS_Label")
+    self:SetText("Label")
+    self:SetContentAlignment(5)
+    self:SetThemePressedColor("Foreground1Color")
+    self:SetThemeHoverColor("Foreground1Color")
+    self:DockPadding(6, 6, 6, 6)
+
+    self:TDLib()
+        :SetupTransition("ButtonDown", 6, downfunc)
+        :SetupTransition("MouseHover", 6, TDLibUtil.HoverFunc)
+end
+
+function PANEL:Paint(w, h)
+    draw.RoundedBox(6, 0, 0, w, h, PS:GetThemeVar("MainColor"))
+    draw.RoundedBox(6, 0, 0, w, h, ColorAlpha(PS:GetThemeVar(self._down), self._downA * self.ButtonDown))
+    draw.RoundedBox(6, 0, 0, w, h, ColorAlpha(PS:GetThemeVar(self._hover), self._hoverA * self.MouseHover))
+end
+
+function PANEL:PaintOver(w, h)
+    local l, t, r, b = self:GetDockPadding()
+
+    local text = self._text
+    local mat = self.IconMaterial
+    local mat_w, mat_h = self.IconWidth, self.IconHeight
+
+    if self:IsHovered() then
+        if self._hovertext ~= nil then
+            text = self._hovertext
+        end
+
+        if self.HoverIconMaterial ~= nil and self.HoverIconMaterial ~= "" then
+            if not self.HoverIconMaterial then
+                mat = nil
+            else
+                mat = self.HoverIconMaterial
+                mat_w, mat_h = self.HoverIconWidth, self.HoverIconHeight
+            end
+        end
+    end
+
+    local x = 0
+    if self.AlignX == TEXT_ALIGN_LEFT then
+        x = l
+    elseif self.AlignX == TEXT_ALIGN_CENTER then
+        x = w * 0.5
+    elseif self.AlignX == TEXT_ALIGN_RIGHT then
+        x = w - r
+    end
+
+    local y = 0
+    if self.AlignY == TEXT_ALIGN_TOP then
+        y = t
+    elseif self.AlignY == TEXT_ALIGN_CENTER then
+        y = h * 0.5
+    elseif self.AlignY == TEXT_ALIGN_BOTTOM then
+        y = h - b
+    end
+
+    if mat ~= nil then
+        if self.AlignX == TEXT_ALIGN_LEFT then
+            x = x + mat_w + 18
+        elseif self.AlignX == TEXT_ALIGN_CENTER then
+            x = x + mat_w * 0.5 + 3
+        end
+    end
+
+    w = PS.ShadowedText(text, self:GetFont(), x, y, COLOR_WHITE, self.AlignX, self.AlignY)
+
+    if mat ~= nil then
+        if self.AlignX == TEXT_ALIGN_CENTER then
+            w = w * 0.5
+        elseif self.AlignX == TEXT_ALIGN_LEFT then
+            w = 0
+        end
+
+        PS.ShadowedImage(mat, x - mat_w - w, y, mat_w, mat_h, COLOR_WHITE, TEXT_ALIGN_CENTER, self.AlignY)
+    end
+end
+
+function PANEL:SetTextOverride(text)
+    self:SetTextOriginal(text)
+    self:SetTextColor(Color(0, 0, 0, 0))
+    self._text = text
+end
+
+local alignsX = { TEXT_ALIGN_LEFT, TEXT_ALIGN_CENTER, TEXT_ALIGN_RIGHT, TEXT_ALIGN_LEFT, TEXT_ALIGN_CENTER, TEXT_ALIGN_RIGHT, TEXT_ALIGN_LEFT, TEXT_ALIGN_CENTER, TEXT_ALIGN_RIGHT }
+local alignsY = { TEXT_ALIGN_BOTTOM, TEXT_ALIGN_BOTTOM, TEXT_ALIGN_BOTTOM, TEXT_ALIGN_CENTER, TEXT_ALIGN_CENTER, TEXT_ALIGN_CENTER, TEXT_ALIGN_TOP, TEXT_ALIGN_TOP, TEXT_ALIGN_TOP }
+function PANEL:SetContentAlignmentOverride(align)
+    self:SetContentAlignmentOriginal(align)
+    self.AlignX = alignsX[align]
+    self.AlignY = alignsY[align]
+end
+
+function PANEL:SetIcon(path, w, h)
+    self.IconWidth = w
+    self.IconHeight = h
+
+    self.IconMaterial = Material(path, "noclamp smooth")
+end
+
+function PANEL:SetHoverIcon(path, w, h)
+    if path == "" or not path then
+        self.HoverIconMaterial = path
+        return
+    end
+
+    self.HoverIconWidth = w
+    self.HoverIconHeight = h
+
+    self.HoverIconMaterial = Material(path, "noclamp smooth")
+end
+
+function PANEL:SetThemePressedColor(color_string, alpha)
+    self._down = color_string
+    self._downA = alpha or 200
+end
+
+function PANEL:SetThemeHoverColor(color_string, alpha)
+    self._hover = color_string
+    self._hoverA = alpha or 125
+end
+
+vgui.Register("PS_Button", PANEL, "DButton")
+
+vgui.Create("PS_Menu")
